670e3bfe13b09d13d30247497555b57c55a69baa
Ravi Patel <ravi.patel@otuindia.com>
2023-09-01 14:08:16 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_strip_demo/strip/strip_payment.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = "pk_test_srIN1InLfZAWqe6L6nEn7w33005oASkg8F";
+  Stripe.merchantIdentifier = 'merchant.com.caritasrevolution';
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const StripPaymentScreen(),
+                  ),
+                );
+              },
+              child: const Text("Stripe Payment"),
+            ),
+            
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/strip/strip_payment.dart
@@ -0,0 +1,68 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:http/http.dart' as http;
+
+class StripPaymentScreen extends StatefulWidget {
+  const StripPaymentScreen({super.key});
+
+  @override
+  State<StripPaymentScreen> createState() => _StripPaymentScreenState();
+}
+
+class _StripPaymentScreenState extends State<StripPaymentScreen> {
+  void onTapDonate(context) async {
+    /*if (getPaymentMethod() == null) {
+      showCommonAlert(
+          context, ALERT_TITLE_APP_NAME, "Please add or select a card.");
+    } else if (double.parse(getTotalAmountToDisplay()) <
+        minimumDonationAmount) {
+      showCommonAlert(context, ALERT_TITLE_APP_NAME,
+          "Donation amount cannot be less than \$$minimumDonationAmount.");
+    } else {
+      makeDonation();
+    }*/
+    var paymentIntent = await createPayment();
+    await Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        paymentIntentClientSecret: paymentIntent!['client_secret'],
+        merchantDisplayName: "Testing",
+      ),
+    );
+    diaplaySheet();
+  }
+
+  diaplaySheet() async {
+    try {
+      await Stripe.instance.presentPaymentSheet().then((value) {
+        print("Success Payment ${value}");
+      });
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  createPayment() async {
+    Map<String, dynamic> map = {
+      "amount": "1000",
+      "currency": "inr",
+      "payment_method_types[]": 'card'
+    };
+    var response =
+        await http.post(Uri.parse("https://api.stripe.com/v1/payment_intents"),
+            headers: {
+              'Authorization':
+                  'Bearer sk_test_fN0nlTWQP1hWQPlBHwWgymyH00cBpfhnnh',
+              'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: map);
+    print(response.body);
+    return jsonDecode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
